868c586129cea94141f531e803c8e8c67d021fbf
Gameunkulus <bugr24-8@mail.ru>
2023-10-16 13:53:38 +0300

    Homework-JS-4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
+
+
 function Student(name, gender, age) {
-  
-}
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+	Student.prototype.setSubject = function(subjectName) {
+		this.subject = subjectName;
+	};
 
-Student.prototype.getAverage = function () {
-  
-}
+	Student.prototype.addMarks = function(...marks) {
+		if (!this.marks) {
+			console.log("Error: Student has been expiled.");
+			return;
+		}
+		this.marks = [...this.marks, ...marks]
+	};
 
-Student.prototype.exclude = function (reason) {
-  
+	Student.prototype.getAverage = function() {
+		if (!this.marks || !this.marks.length) {
+			return 0;
+		}
+		const sum = this.marks.reduce((acc, curr) => acc + curr);
+		return sum / this.marks.length;
+	};
+
+	Student.prototype.exclude = function(reason) {
+		delete this.subject;
+		delete this.marks;
+		this.excluded = reason;
+	};
 }
