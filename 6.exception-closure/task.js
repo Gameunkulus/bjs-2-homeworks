f50ecaf19cd87d7b4749e0f04e1986395272057e
Gameunkulus <bugr24-8@mail.ru>
2023-10-19 23:36:28 +0300

    Homework-JS-6

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,55 @@
-﻿
+﻿'use strict';
+function parseCount(value) {
+	const result = parseFloat(value);
+	if (isNaN(result)) {
+		throw new Error("Невалидное значение");
+	}
+	return result;
+}
+
+function validateCount(value) {
+	try {
+		return parseCount(value);
+	} catch (e) {
+		return e;
+	}
+}
+
+class Triangle {
+
+	constructor(sideA, sideB, sideC) {
+		if (sideA + sideB < sideC || sideC + sideA < sideB || sideB + sideC < sideA) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+		this.sideA = sideA;
+		this.sideB = sideB;
+		this.sideC = sideC;
+	}
+
+	get perimeter() {
+
+		return this.sideA + this.sideB + this.sideC;
+
+	}
+
+	get area() {
+		let halfPer = this.perimeter / 2;
+		let result = +Math.sqrt(halfPer * (halfPer - this.sideA) * (halfPer - this.sideB) * (halfPer - this.sideC)).toFixed(3);
+		return result;
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (e) {
+		return {
+			get perimeter() {
+				return "Ошибка! Треугольник не существует";
+			},
+			get area() {
+				return "Ошибка! Треугольник не существует";
+			}
+		}
+	}
+}
